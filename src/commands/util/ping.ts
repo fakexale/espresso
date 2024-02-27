6732926b48bc21cbe1c021f380be94539686ddd8
fakexale <136240363+fakexale@users.noreply.github.com>
2024-02-27 16:47:55 +0100

    refactor: explicitly set dm permission

--- src/commands/util/ping.ts
+++ src/commands/util/ping.ts
@@ -3,7 +3,8 @@ import type { CommandInteraction } from "discord.js";
 
 export const data = new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Queries the bot");
+    .setDescription("Queries the bot")
+    .setDMPermission(true);
 
 export const execute = async (interaction: CommandInteraction) => {
     await interaction.reply("Pong!");
